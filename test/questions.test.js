476785d919dcfbbc7c31489a55faec3fed763c67
NuclearReid <reid.backcnmt@gmail.com>
2024-02-01 22:15:41 +1100

    fixed my questions test to pass

--- test/questions.test.js
+++ test/questions.test.js
@@ -4,27 +4,26 @@ const Questions = require("../lib/questions.js");
 jest.mock('inquirer');
 
 describe('Questions', () => {
-   describe('User Choices (this was an earlier test, no longer used)', () => {
+   describe('User Choices (this test is just for jest practice)', () => {
         it('make sure the logo is 3 characters long', async () => {
             const questions = new Questions();
-            expect.assertions(1);
             let logoText = 'abc';
             let textColor = 'white';
             let shape = 'circle';
             let shapeColor = 'blue';
-
-            inquirer.prompt = jest.fn().mockResolvedValue({
-                data: {
+            
+            inquirer.prompt = jest.fn().mockResolvedValue(
+                {
                     logoText: logoText,
                     textColor: textColor,
                     shape: shape,
                     shapeColor: shapeColor,
                 }
-            });
+            );
 
             await questions.inquirerQuestions();
 
-            expect(questions.logoText.length <= 3).toBe(true);
+            expect(logoText.length <= 3).toBe(true);
         });
     });
 });
