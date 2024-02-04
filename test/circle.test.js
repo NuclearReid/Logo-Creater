4487ff749d306abefa387c8da41a7c8c5a4e9d49
NuclearReid <reid.backcnmt@gmail.com>
2024-02-05 10:44:58 +1100

    adjusted text placement on the circle and fixed triangle & circle test

--- test/circle.test.js
+++ test/circle.test.js
@@ -10,7 +10,7 @@ describe('Circle', () =>{
         const shapeColor = 'blue';
 
         const circle = new Circle(logoText, textColor, shape, shapeColor);
-        expect(circle.render()).toBe(`<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg"><circle cx="100" cy="100" r="100" fill="blue"/><text x="25%" y="55%" font-size="50px" fill="white">mdn</text></svg>`);
+        expect(circle.render()).toBe(`<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg"><circle cx="100" cy="100" r="100" fill="blue"/><text x="15%" y="55%" font-size="50px" fill="white">mdn</text></svg>`);
 
     });
 });
--- test/triangle.test.js
+++ test/triangle.test.js
@@ -9,7 +9,7 @@ describe('Triangle', () =>{
         const shapeColor = 'blue';
         const triangle = new Triangle(logoText, textColor, shape, shapeColor);
        
-        expect(triangle.render()).toBe(`<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg"><polygon points="300,0 0,0 150,200" fill="blue"/><text x="40%" y="40%" font-size="50px" fill="white">mdn</text></svg>`);
+        expect(triangle.render()).toBe(`<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg"><polygon points="300,0 0,0 150,200" fill="blue"/><text x="30%" y="40%" font-size="50px" fill="white">mdn</text></svg>`);
 
     });
 });
